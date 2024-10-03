ebeee759a7824025dd5a28b34e89c9a5d1865b32
devarsh-developer <devarshsathiya@gmail.com>
2024-10-04 00:45:22 +0530

    number

---
+++ num.py
@@ -0,0 +1,20 @@
+num= 11+11
+print(num)
+
+num= 1333432-498690
+print(num)
+
+num= 474475+6486
+print(num)
+
+num= 1000+50077
+print(num)
+
+num= 5%5
+print(num)
+
+num= 12*12
+print(num)
+
+num= 520/2
+print(num)
